19de591a3fa8086b7fbcf23918d6e9338670db30
chenrujun97 <chenrujun97@qq.com>
2019-06-02 14:59:39 +0800

    question paging

--- src/main/java/com/crj/hello/controller/IndexController.java
+++ src/main/java/com/crj/hello/controller/IndexController.java
@@ -1,15 +1,15 @@
 package com.crj.hello.controller;
 
+import com.crj.hello.dto.PaginationDTO;
 import com.crj.hello.dto.QuestionDTO;
-import com.crj.hello.mapper.QuestionMapper;
 import com.crj.hello.mapper.UserMapper;
-import com.crj.hello.model.Question;
 import com.crj.hello.model.User;
 import com.crj.hello.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -26,7 +26,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
@@ -41,8 +43,8 @@ public class IndexController {
             }
         }
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions", questionList);
+        PaginationDTO pagination = questionService.list(page, size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
